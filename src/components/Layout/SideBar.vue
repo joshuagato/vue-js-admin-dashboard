e19b570a6273e20e86e5f2c7f02d80186387d0d1
Joshua Gato <joshuagato37@gmail.com>
2025-06-19 01:45:07 +0000

    Changed the layout to conform to the design

--- src/components/Layout/SideBar.vue
+++ src/components/Layout/SideBar.vue
@@ -8,20 +8,24 @@ export default defineComponent({
 
 <template>
   <section id="side-bar">
-    <article class="menu-item"><img src="../../assets/side-menu/users-solid%201.png" /><span>Users</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/video-solid%202.png" /><span>Bookings</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/user-graduate-solid 1.png" /><span>Academics</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/wallet-solid 1.png" /><span>Finance</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/user-graduate-solid 1.png" /><span>Academy</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/user-graduate-solid 1.png" /><span>Schools</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/bullhorn-solid 1.png" /><span>Marketing</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/message-solid 1.png" /><span>Messages</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/envelope-solid 2.png" /><span>Emails</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/robot-solid 1.png" /><span>Support</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/paintbrush-solid 1.png" /><span>Design</span></article>
-    <article class="menu-item"><img src="../../assets/side-menu/star-solid 17.png" /><span>Reviews</span></article>
+    <section id="side-bar-top">
+      <article class="menu-item"><img src="../../assets/side-menu/users-solid%201.png" /><span>Users</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/video-solid%202.png" /><span>Bookings</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/user-graduate-solid 1.png" /><span>Academics</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/wallet-solid 1.png" /><span>Finance</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/user-graduate-solid 1.png" /><span>Academy</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/user-graduate-solid 1.png" /><span>Schools</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/bullhorn-solid 1.png" /><span>Marketing</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/message-solid 1.png" /><span>Messages</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/envelope-solid 2.png" /><span>Emails</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/robot-solid 1.png" /><span>Support</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/paintbrush-solid 1.png" /><span>Design</span></article>
+      <article class="menu-item"><img src="../../assets/side-menu/star-solid 17.png" /><span>Reviews</span></article>
+    </section>
 
-    <article class="menu-item"><img src="../../assets/side-menu/right-from-bracket-solid 1.png" /></article>
+    <section id="side-bar-bottom">
+      <article class="menu-item"><img src="../../assets/side-menu/right-from-bracket-solid 1.png" /></article>
+    </section>
   </section>
 </template>
 
@@ -34,6 +38,9 @@ export default defineComponent({
   font-size: 16px;
   font-weight: 600;
   font-family: Inter, sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   .menu-item {
     margin-bottom: 34px;
@@ -48,14 +55,16 @@ export default defineComponent({
     }
   }
 
-  .menu-item:first-child {
-    padding-top: 34px;
+  #side-bar-top {
+    .menu-item:first-child {
+      padding-top: 34px;
+    }
   }
 
-  .menu-item:last-child {
-    justify-content: center;
-    margin-bottom: 0;
-    padding-left: 0;
+  #side-bar-bottom {
+    .menu-item {
+      justify-content: center;
+    }
   }
 }
 </style>
