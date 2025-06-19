2b523606560d777779175a88c31b2101e48a02d5
Joshua Gato <joshuagato37@gmail.com>
2025-06-19 13:06:03 +0000

    Added cards

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,21 +2,36 @@
 import {defineComponent} from 'vue'
 import TopBar from "./Layout/TopBar.vue";
 import SideBar from "./Layout/SideBar.vue";
+import DisplayCard from "./DisplayCard.vue";
 
 export default defineComponent({
   name: "Layout",
-  components: {SideBar, TopBar}
+  components: {SideBar, TopBar, DisplayCard}
 })
 </script>
 
 <template>
   <div id="main-layout">
     <TopBar />
-
     <SideBar />
+
+    <section id="main-content-section">
+      <DisplayCard :data-figure="124" data-text="Student Signups" />
+      <DisplayCard :data-figure="124" data-text="Upcoming Lessons" />
+      <DisplayCard :data-figure="124" data-text="Total Tickets" />
+    </section>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+#main-content-section {
+  background-color: #EEFBF2;
+  position: absolute;
+  top: 73px;
+  left: 172px;
+  font-family: Inter, sans-serif;
+  width: calc(100% - 172px);
+  height: calc(100% - 73px);
+  display: flex;
+}
 </style>
