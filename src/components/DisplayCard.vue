617e7a8334dfa0409ed6b3c9493884b04fdc3407
Joshua Gato <joshuagato37@gmail.com>
2025-06-19 12:45:24 +0000

    Added display card component

---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+defineProps({
+  dataFigure: Number,
+  dataText: String
+})
+</script>
+
+<template>
+  <article class="data-card">
+    <span class="data-figure">{{ dataFigure }}</span>
+    <span class="data-text">{{ dataText }}</span>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.data-card {
+  width: 273px;
+  height: 134px;
+  background-color: #FFF;
+  border-radius: 10px;
+  margin-top: 34px;
+  margin-left: 34px;
+  text-align: center;
+
+  .data-figure {
+    font-size: 60px;
+    padding-top: 18px;
+  }
+
+  .data-text {
+    font-size: 14px;
+    margin-top: 12px;
+  }
+
+  .data-figure, .data-text {
+    display: block;
+    font-weight: 500;
+  }
+}
+</style>
