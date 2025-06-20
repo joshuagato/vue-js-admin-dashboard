7932d5500622504546b168a2ef5052077f7ed860
Joshua Gato <joshuagato37@gmail.com>
2025-06-20 13:54:37 +0000

    Added staff component

---
+++ src/components/users/Staff.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import DisplayCard from "../DisplayCard.vue";
+import FilterCard from "../FilterCard.vue";
+
+</script>
+
+<template>
+  <article id="staff-container">
+    <DisplayCard :data-figure="124" data-text="Total Staff" />
+    <DisplayCard :data-figure="12" data-text="Active Staff" />
+    <DisplayCard :data-figure="12" data-text="Deactivated Staff" />
+
+    <FilterCard filter-title="All Data" filter-option1="Option 1" filter-option2="Option 2" filter-option3="Option 3" />
+  </article>
+</template>
+
+<style scoped lang="scss">
+#staff-container {
+  display: flex;
+
+
+}
+
+
+</style>
