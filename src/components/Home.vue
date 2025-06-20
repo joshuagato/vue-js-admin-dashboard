20b02a5d6662fad3d6eb684c60652a5a0c86698f
Joshua Gato <joshuagato37@gmail.com>
2025-06-20 08:48:43 +0000

    Created a home component and outsourced the display cards in it

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import DisplayCard from "./DisplayCard.vue";
+
+</script>
+
+<template>
+  <article>
+    <DisplayCard :data-figure="124" data-text="Student Signups" />
+    <DisplayCard :data-figure="124" data-text="Upcoming Lessons" />
+    <DisplayCard :data-figure="124" data-text="Total Tickets" />
+  </article>
+</template>
+
+<style scoped lang="scss">
+
+</style>
