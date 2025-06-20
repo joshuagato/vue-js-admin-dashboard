8170ddd16292bfa2c5af9d46437ebf9064c55ce8
Joshua Gato <joshuagato37@gmail.com>
2025-06-20 19:50:15 +0000

    Finished working on the top section

--- src/components/users/Staff.vue
+++ src/components/users/Staff.vue
@@ -6,19 +6,80 @@ import FilterCard from "../FilterCard.vue";
 
 <template>
   <article id="staff-container">
-    <DisplayCard :data-figure="124" data-text="Total Staff" />
-    <DisplayCard :data-figure="12" data-text="Active Staff" />
-    <DisplayCard :data-figure="12" data-text="Deactivated Staff" />
+    <section class="top-section">
+      <DisplayCard :data-figure="124" data-text="Total Staff" />
+      <DisplayCard :data-figure="12" data-text="Active Staff" />
+      <DisplayCard :data-figure="12" data-text="Deactivated Staff" />
 
-    <FilterCard filter-title="All Data" filter-option1="Option 1" filter-option2="Option 2" filter-option3="Option 3" />
+      <FilterCard filter-title="All Data" filter-option1="Option 1" filter-option2="Option 2" filter-option3="Option 3" />
+    </section>
+
+    <section class="bottom-section">
+      <article class="head-section">
+        <h1 class="heading">{{'Staff'}}</h1>
+        <router-link to="">{{'Create Staff'}}</router-link>
+      </article>
+      <hr />
+
+    </section>
   </article>
 </template>
 
 <style scoped lang="scss">
 #staff-container {
-  display: flex;
+  .top-section {
+    display: flex;
+  }
 
+  .bottom-section {
+    width: 1160px;
+    height: calc(100vh - 50px - 134px - 34px - 73px);
+    background-color: #FFFFFF;
+    margin-top: 50px;
+    margin-left: 40px;
+    border-radius: 10px;
+    font-family: Inter, sans-serif;
 
+    .head-section {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+
+      .heading {
+        font-size: 20px;
+        font-weight: 600;
+        padding-top: 28px;
+        margin-bottom: 28px;
+        margin-left: 45px;
+      }
+
+      a {
+        width: 137px;
+        height: 36px;
+        border-radius: 5px;
+        background-color: #0C7334;
+        color: #FFF;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 700;
+        font-size: 14px;
+        margin-right: 50px;
+
+        &:link, &:visited {
+          color: #FFF;
+        }
+      }
+    }
+
+    hr {
+      height: 2px;
+      color: #D9D9D9;
+      background-color: #D9D9D9;
+    }
+  }
 }
 
 
