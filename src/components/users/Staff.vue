daa381b6d09c00e39b655d3da2cbd0d8aba4f845
Joshua Gato <joshuagato37@gmail.com>
2025-06-20 21:38:56 +0000

    Done with the main content

--- src/components/users/Staff.vue
+++ src/components/users/Staff.vue
@@ -31,6 +31,34 @@ import FilterCard from "../FilterCard.vue";
         />
       </article>
 
+      <article class="table-section">
+        <table>
+          <thead>
+            <tr>
+              <th style="width: 100px">ID NO</th>
+              <th>Full Name</th>
+              <th>Department</th>
+              <th>Role</th>
+              <th style="width: 140px">Date Created</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td style="width: 100px">VT002123</td>
+              <td>Karen Okyere</td>
+              <td>Tech</td>
+              <td>Technical Agent</td>
+              <td style="width: 140px">23/09/2023</td>
+              <td>
+                <button class="view-more">View Staff</button>
+                <button class="activate">Activate</button>
+                <button class="deactivate">Deactivate</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
     </section>
   </article>
 </template>
@@ -42,7 +70,7 @@ import FilterCard from "../FilterCard.vue";
   }
 
   .bottom-section {
-    width: 1160px;
+    max-width: 1400px;
     height: calc(100vh - 50px - 134px - 34px - 73px);
     background-color: #FFFFFF;
     margin-top: 50px;
@@ -97,6 +125,56 @@ import FilterCard from "../FilterCard.vue";
 
       .filter-card {
         margin-right: 66px;
+      }
+    }
+
+    .table-section {
+      margin-left: 54px;
+
+      th {
+        font-weight: 700;
+        font-size: 18px;
+        width: 220px;
+        text-align: left;
+      }
+
+      td {
+        font-weight: 400;
+        font-size: 14px;
+        width: 220px;
+        text-align: left;
+      }
+
+      tbody {
+        td {
+          &:last-child {
+            width: 320px;
+            display: flex;
+            justify-content: space-between;
+
+            button {
+              font-weight: 500;
+              font-size: 14px;
+              border-radius: 5px;
+              width: 100px;
+              height: 34px;
+              border: none;
+              cursor: pointer;
+            }
+
+            .view-more {
+              background-color: #DADADA;
+            }
+
+            .activate {
+              background-color: #9CFFC5;
+            }
+
+            .deactivate {
+              background-color: #FE9E9E;
+            }
+          }
+        }
       }
     }
 
