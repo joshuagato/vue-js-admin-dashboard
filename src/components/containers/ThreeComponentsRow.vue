e856a5de899514e1383683ab87d1f140b76164c7
Joshua Gato <joshuagato37@gmail.com>
2025-06-22 17:47:07 +0000

    Added three components row component

---
+++ src/components/containers/ThreeComponentsRow.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="three-component-row">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.three-component-row {
+  margin-left: 80px;
+  width: calc(100% - 80px - 80px);
+  display: flex;
+  justify-content: space-between;
+}
+</style>
