d239fb2423ad5821eb33aa0d239a4225de3d3b27
Joshua Gato <joshuagato37@gmail.com>
2025-06-23 09:53:26 +0000

    Added diving header component

---
+++ src/components/headers/DividingHeader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  text: String,
+})
+</script>
+
+<template>
+  <div class="heading-container">
+    <h2 class="heading">{{text}}</h2>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.heading-container {
+  margin-left: 80px;
+  margin-bottom: 40px;
+  margin-top: 100px;
+
+  .heading {
+    font-weight: 700;
+    font-size: 19px;
+  }
+}
+</style>
