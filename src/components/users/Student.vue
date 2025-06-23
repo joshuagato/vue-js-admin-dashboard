5ed70f657df6e600e1c2cb88d790c3fe0108bc38
Joshua Gato <joshuagato37@gmail.com>
2025-06-23 19:03:57 +0000

    Added student statistics cards

---
+++ src/components/users/Student.vue
@@ -0,0 +1,240 @@
+<script setup lang="ts">
+import DisplayCard from "../DisplayCard.vue";
+import FilterCard from "../FilterCard.vue";
+
+const staffList = [
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+  { id: 'VT002123', fullName: 'Karen Okyere', department: 'Tech', role: 'Technical Agent', dateCreated: '23/09/2023' },
+]
+
+</script>
+
+<template>
+  <article id="staff-container" class="fade-in">
+    <section class="top-section">
+      <FilterCard filter-title="All Data" filter-option1="Option 1" filter-option2="Option 2" filter-option3="Option 3" />
+    </section>
+
+    <div class="middle-section">
+      <article class="row">
+
+      </article>
+      <article class="row">
+        <DisplayCard :data-figure="124" data-text="Total Students" />
+        <DisplayCard :data-figure="124" data-text="Active Students" />
+        <DisplayCard :data-figure="124" data-text="Deactivated Students" />
+        <DisplayCard :data-figure="124" data-text="Private Students" />
+      </article>
+      <article class="row">
+        <DisplayCard :data-figure="124" data-text="Active Private Students" />
+        <DisplayCard :data-figure="124" data-text="Deactivated Private Students" />
+        <DisplayCard :data-figure="124" data-text="InPerson Students" />
+        <DisplayCard :data-figure="124" data-text="Active InPerson Students" />
+      </article>
+      <article class="row">
+        <DisplayCard :data-figure="124" data-text="Deactivated InPerson Students" />
+        <DisplayCard :data-figure="124" data-text="Academy Students" />
+        <DisplayCard :data-figure="124" data-text="Active Academy Students" />
+        <DisplayCard :data-figure="124" data-text="Deactivated Academy Students" />
+      </article>
+      <article class="row">
+        <DisplayCard :data-figure="124" data-text="School Opportunity" />
+        <DisplayCard :data-figure="124" data-text="Active School Opportunity" />
+        <DisplayCard :data-figure="124" data-text="Deactivated School Opportunity" />
+      </article>
+    </div>
+
+    <section class="bottom-section">
+      <article class="head-section">
+        <h1 class="heading">{{'Staff'}}</h1>
+        <router-link to="/users/create-staff">{{'Create Staff'}}</router-link>
+      </article>
+
+      <hr />
+
+      <article class="mid-section">
+        <h1 class="heading">Total Number: {{'200'}}</h1>
+        <FilterCard
+            class="filter-card" filter-title="All Accounts"
+            filter-option1="Option 1" filter-option2="Option 2"
+            filter-option3="Option 3"
+        />
+      </article>
+
+      <article class="table-section">
+        <table>
+          <thead>
+            <tr>
+              <th style="width: 100px">ID NO</th>
+              <th>Full Name</th>
+              <th>Department</th>
+              <th>Role</th>
+              <th style="width: 140px">Date Created</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="staff in staffList">
+              <td style="width: 100px">{{staff.id}}</td>
+              <td>{{staff.fullName}}</td>
+              <td>{{ staff.department }}</td>
+              <td>{{staff.role}}</td>
+              <td style="width: 140px">{{staff.dateCreated}}</td>
+              <td>
+                <button class="view-more">View Staff</button>
+                <button class="activate">Activate</button>
+                <button class="deactivate">Deactivate</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
+    </section>
+  </article>
+</template>
+
+<style scoped lang="scss">
+#staff-container {
+  .top-section {
+    max-width: 1400px;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .middle-section {
+    max-width: 1400px;
+
+    .row {
+      width: 100%;
+      display: flex;
+    }
+  }
+
+  .bottom-section {
+    max-width: 1400px;
+    height: calc(100vh - 50px - 134px - 34px - 73px);
+    background-color: #FFFFFF;
+    margin-top: 50px;
+    margin-left: 40px;
+    border-radius: 10px;
+    font-family: Inter, sans-serif;
+
+    .head-section {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+
+      .heading {
+        font-size: 20px;
+        font-weight: 600;
+        padding-top: 28px;
+        margin-bottom: 28px;
+        margin-left: 45px;
+      }
+
+      a {
+        width: 137px;
+        height: 36px;
+        border-radius: 5px;
+        background-color: #0C7334;
+        color: #FFF;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 700;
+        font-size: 14px;
+        margin-right: 50px;
+
+        &:link, &:visited {
+          color: #FFF;
+        }
+      }
+    }
+
+    .mid-section {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .heading {
+        font-size: 18px;
+        font-weight: 700;
+        margin-left: 54px;
+      }
+
+      .filter-card {
+        margin-right: 66px;
+      }
+    }
+
+    .table-section {
+      margin-left: 54px;
+
+      th {
+        font-weight: 700;
+        font-size: 18px;
+        width: 220px;
+        text-align: left;
+      }
+
+      td {
+        font-weight: 400;
+        font-size: 14px;
+        width: 220px;
+        text-align: left;
+      }
+
+      tbody {
+        tr {
+          td {
+            border-bottom: 1px solid #D9D9D9;
+            &:last-child {
+              width: 320px;
+              display: flex;
+              justify-content: space-between;
+
+              button {
+                font-weight: 500;
+                font-size: 14px;
+                border-radius: 5px;
+                width: 100px;
+                height: 34px;
+                border: none;
+                cursor: pointer;
+              }
+
+              .view-more {
+                background-color: #DADADA;
+              }
+
+              .activate {
+                background-color: #9CFFC5;
+              }
+
+              .deactivate {
+                background-color: #FE9E9E;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    hr {
+      height: 2px;
+      color: #D9D9D9;
+      background-color: #D9D9D9;
+    }
+  }
+}
+
+
+</style>
