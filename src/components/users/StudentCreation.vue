a726733e11bfb285d2be72971be40f37c636e979
Joshua Gato <joshuagato37@gmail.com>
2025-06-23 21:23:18 +0000

    Done with the student info section

---
+++ src/components/users/StudentCreation.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import TextInput from "../inputs/TextInput.vue";
+import EmailInput from "../inputs/EmailInput.vue";
+import ThreeComponentsRow from "../containers/ThreeComponentsRow.vue";
+import TelephoneInput from "../inputs/TelephoneInput.vue";
+import PasswordInput from "../inputs/PasswordInput.vue";
+import DateInput from "../inputs/DateInput.vue";
+import DropDown from "../inputs/DropDown.vue";
+import TwoComponentsRow from "../containers/TwoComponentsRow.vue";
+import DividingHeader from "../headers/DividingHeader.vue";
+import FirstSubHeading from "../headers/FirstSubHeading.vue";
+import FileInput from "../inputs/FileInput.vue";
+import TwoComponentRow from "../containers/TwoComponentRow.vue";
+import CreateEntityButton from "../buttons/CreateEntityButton.vue";
+import MainHeading from "../headers/MainHeading.vue";
+import MainAndFirstHeading from "../headers/MainAndFirstHeading.vue";
+
+const options = [
+  { label: "IT Support", value: "IT Support" },
+  { label: "IT Support", value: "IT Support" },
+  { label: "IT Support", value: "IT Support" },
+];
+
+</script>
+
+<template>
+<div id="staff-creation-container" class="fade-in">
+  <MainAndFirstHeading text="Student Info." />
+
+  <ThreeComponentsRow>
+    <TextInput id="firstName" label="First Name" />
+    <TextInput id="lastName" label="Last Name" />
+    <TextInput id="username" label="Username" />
+  </ThreeComponentsRow>
+
+  <ThreeComponentsRow>
+    <EmailInput id="email" label="Email" />
+    <PasswordInput id="password" label="Password" />
+    <PasswordInput id="confirmPassword" label="Confirm Password" />
+  </ThreeComponentsRow>
+
+  <ThreeComponentsRow>
+    <TelephoneInput id="phoneNumber" label="Phone Number" />
+    <DateInput id="dateOfBirth" label="Date of Birth" />
+    <DropDown id="gender" label="Select Gender" default-option="Select Gender" :options="options" model-value="Select Gender" />
+  </ThreeComponentsRow>
+
+  <ThreeComponentsRow>
+    <TextInput id="country" label="Country of Residence" />
+    <DropDown id="curriculum" label="Select Curriculum" default-option="Select Curriculum" :options="options" model-value="Select Curriculum" />
+    <DropDown id="studentType" label="Select Student Type" default-option="Select Student Type" :options="options" model-value="Select Student Type" />
+  </ThreeComponentsRow>
+
+
+
+  <!-- Submit Button -->
+  <CreateEntityButton title="Create Student" />
+</div>
+</template>
+
+<style scoped lang="scss">
+#staff-creation-container {
+  max-width: 1400px;
+  height: calc(100vh - 50px - 73px);
+  background-color: #FFFFFF;
+  margin-top: 50px;
+  margin-left: 40px;
+  border-radius: 10px;
+  overflow-y: scroll;
+}
+</style>
