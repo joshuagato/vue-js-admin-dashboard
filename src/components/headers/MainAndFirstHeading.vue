b5850e3eae4f4986f9a5cde28e3a0ad8211cbdae
Joshua Gato <joshuagato37@gmail.com>
2025-06-23 21:18:03 +0000

    Added main and first heading component

---
+++ src/components/headers/MainAndFirstHeading.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="main-heading-container">
+    <h1 class="main-heading">{{text}}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main-heading-container {
+  margin-top: 65px;
+  margin-bottom: 65px;
+  margin-left: 80px;
+
+  .main-heading {
+    font-weight: 700;
+    font-size: 23px;
+  }
+}
+</style>
