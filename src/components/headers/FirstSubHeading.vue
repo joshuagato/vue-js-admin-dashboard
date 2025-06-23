fbc79940e429deb8b1aa9c90f1310b0e003652c4
Joshua Gato <joshuagato37@gmail.com>
2025-06-23 10:08:24 +0000

    Added first sub-heading component

---
+++ src/components/headers/FirstSubHeading.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="heading-container">
+    <h3 class="sub-heading">{{text}}</h3>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.heading-container {
+
+  margin-top: 80px;
+  margin-left: 80px;
+  margin-bottom: 21px;
+
+  .sub-heading {
+    font-weight: 700;
+    font-size: 19px;
+  }
+}
+</style>
