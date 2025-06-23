13630e201ccc51865528ca0cdd426e38cb5ddfe4
Joshua Gato <joshuagato37@gmail.com>
2025-06-23 10:53:19 +0000

    Two component row component

---
+++ src/components/containers/TwoComponentRow.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="two-component-row">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.two-component-row {
+  margin-left: 80px;
+  margin-bottom: 40px;
+  width: calc(100% - 80px - 80px);
+  display: flex;
+  justify-content: space-between;
+}
+</style>
