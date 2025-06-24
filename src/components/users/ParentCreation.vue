ce7de40f0fadad147c56296bb9af348ebe8d1e97
Joshua Gato <joshuagato37@gmail.com>
2025-06-24 10:58:00 +0000

    Done with create parent component

---
+++ src/components/users/ParentCreation.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import TextInput from "../inputs/TextInput.vue";
+import EmailInput from "../inputs/EmailInput.vue";
+import ThreeComponentsRow from "../containers/ThreeComponentsRow.vue";
+import TelephoneInput from "../inputs/TelephoneInput.vue";
+import PasswordInput from "../inputs/PasswordInput.vue";
+import DateInput from "../inputs/DateInput.vue";
+import DropDown from "../inputs/DropDown.vue";
+import TwoComponentsRow from "../containers/TwoComponentsRow.vue";
+import DividingHeader from "../headers/DividingHeader.vue";
+import FirstSubHeading from "../headers/FirstSubHeading.vue";
+import FileInput from "../inputs/FileInput.vue";
+import TwoComponentRow from "../containers/TwoComponentRow.vue";
+import CreateEntityButton from "../buttons/CreateEntityButton.vue";
+import MainHeading from "../headers/MainHeading.vue";
+import MainAndFirstHeading from "../headers/MainAndFirstHeading.vue";
+import DropDownLong from "../inputs/DropDownLong.vue";
+import OneComponentRow from "../containers/OneComponentRow.vue";
+
+const options = [
+  { label: "IT Support", value: "IT Support" },
+  { label: "IT Support", value: "IT Support" },
+  { label: "IT Support", value: "IT Support" },
+];
+
+</script>
+
+<template>
+<div id="staff-creation-container" class="fade-in">
+  <MainAndFirstHeading text="Parent Info." />
+
+  <ThreeComponentsRow>
+    <TextInput id="firstName" label="First Name" />
+    <TextInput id="lastName" label="Last Name" />
+    <TextInput id="username" label="Username" />
+  </ThreeComponentsRow>
+
+  <ThreeComponentsRow>
+    <EmailInput id="email" label="Email" />
+    <TelephoneInput id="phoneNumber" label="Phone Number" />
+    <PasswordInput id="password" label="Password" />
+  </ThreeComponentsRow>
+
+  <ThreeComponentsRow>
+    <PasswordInput id="confirmPassword" label="Confirm Password" />
+  </ThreeComponentsRow>
+
+  <!-- Submit Button -->
+  <CreateEntityButton title="Create Parent" />
+</div>
+</template>
+
+<style scoped lang="scss">
+#staff-creation-container {
+  max-width: 1400px;
+  height: calc(100vh - 50px - 73px);
+  background-color: #FFFFFF;
+  margin-top: 50px;
+  margin-left: 40px;
+  border-radius: 10px;
+  overflow-y: scroll;
+}
+</style>
