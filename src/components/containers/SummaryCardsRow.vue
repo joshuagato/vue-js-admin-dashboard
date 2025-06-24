945c450b08933379d58b9cacfa76d2b15973c0df
Joshua Gato <joshuagato37@gmail.com>
2025-06-24 10:01:45 +0000

    Added summary card row components

---
+++ src/components/containers/SummaryCardsRow.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <article class="row">
+    <slot></slot>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.row {
+  width: 100%;
+  display: flex;
+}
+</style>
