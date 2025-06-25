e4700b91ed958c3dca9763557eb8091257744b18
Joshua Gato <joshuagato37@gmail.com>
2025-06-25 19:10:32 +0000

    Added checkboxes container component

---
+++ src/components/containers/CheckBoxesComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  heading: String
+})
+</script>
+
+<template>
+  <section class="checkbox-section">
+    <h3 class="checkbox-section-heading">{{ heading }}</h3>
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.checkbox-section {
+  .checkbox-section-heading {
+    font-size: 20px;
+    font-weight: 500;
+    margin-bottom: 25px;
+  }
+}
+</style>
