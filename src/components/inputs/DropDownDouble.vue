04ef875ba6bae0a1795b948e0c4caafcd45eaef6
Joshua Gato <joshuagato37@gmail.com>
2025-06-25 11:36:11 +0000

    Added drop down double component

---
+++ src/components/inputs/DropDownDouble.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+  id: String,
+  label: String,
+  defaultOption: String,
+  options: Array<T>,
+  modelValue: String,
+})
+
+const internalValue = ref(props.modelValue);
+
+// - Declare the 'update:modelValue' event
+const emit = defineEmits(['update:modelValue'])
+
+// Watch for changes to the internalValue
+watch(internalValue, (newValue) => {
+  // When internalValue changes, emit the 'update:modelValue' event
+  // with the new value. The parent component's v-model will catch this.
+  emit('update:modelValue', newValue)
+})
+
+// Optionally, watch for changes to the modelValue prop itself.
+// This is important if the parent updates modelValue programmatically
+// and the internal input should reflect that external change.
+watch(() => props.modelValue, (newPropValue) => {
+  if (newPropValue !== internalValue.value) { // Prevent infinite loop if already in sync
+    internalValue.value = newPropValue;
+  }
+});
+
+</script>
+
+<template>
+  <article class="dropdown-container">
+    <label class="label" :for="id">{{label}}</label>
+    <select :id="id" v-model="internalValue">
+      <option selected :value="defaultOption">{{ defaultOption }}</option>
+      <option v-for="{value, label} in options" :value="value">{{label}}</option>
+    </select>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.dropdown-container {
+  display: flex;
+  flex-direction: column;
+
+  .label {
+    font-size: 20px;
+    font-weight: 500;
+    margin-bottom: 15px;
+  }
+
+  select {
+    width: calc(700px + 80px + 10px);
+    height: 68px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 10px;
+    border: 3px solid #716F6F;
+    outline: none;
+    font-size: 18px;
+  }
+}
+</style>
