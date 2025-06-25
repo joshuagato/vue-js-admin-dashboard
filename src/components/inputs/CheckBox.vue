8d322e87db22f6169b6ef98cc6cea05583256c2d
Joshua Gato <joshuagato37@gmail.com>
2025-06-25 19:12:07 +0000

    Added checkbox component

---
+++ src/components/inputs/CheckBox.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+defineProps({
+  id: String,
+  label: String,
+})
+
+</script>
+
+<template>
+  <article class="checkbox">
+    <input :id="id" type="checkbox" :value="label" />
+    <label :for="id">{{ label }}</label>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.checkbox {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  margin-bottom: 10px;
+
+  label {
+    font-size: 16px;
+    font-weight: 500;
+  }
+}
+</style>
