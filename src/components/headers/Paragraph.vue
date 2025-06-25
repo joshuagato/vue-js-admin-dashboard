4205021c73fca034624b9b4ebebbf812d45145d2
Joshua Gato <joshuagato37@gmail.com>
2025-06-25 20:22:45 +0000

    Added paragraph component

---
+++ src/components/headers/Paragraph.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="paragraph-container">
+    <p class="paragraph"><slot></slot></p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.paragraph-container {
+  margin-left: 80px;
+  margin-bottom: 25px;
+
+  .paragraph {
+    font-weight: 500;
+    font-size: 16px;
+  }
+}
+</style>
