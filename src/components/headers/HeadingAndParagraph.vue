9328225b94c6f56174109d80de6770de023268f9
Joshua Gato <joshuagato37@gmail.com>
2025-06-25 21:20:13 +0000

    Added heading and paragraph component

---
+++ src/components/headers/HeadingAndParagraph.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  heading: String,
+  paragraph: String,
+})
+</script>
+
+<template>
+  <div class="heading-and-paragraph-container">
+    <h3 class="heading">{{heading}}</h3>
+    <p class="paragraph">{{paragraph}}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.heading-and-paragraph-container {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  .heading {
+    font-weight: 700;
+    font-size: 20px;
+  }
+
+  .paragraph {
+    font-weight: 500;
+    font-size: 17px;
+  }
+}
+</style>
