97ef1edf94c6a6d926f00575e33fb02ad61f5498
Joshua Gato <joshuagato37@gmail.com>
2025-06-25 20:22:17 +0000

    Added empty space component

---
+++ src/components/headers/EmptySpaceDivider.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <article>
+      <p>&nbsp;</p>
+    </article>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  display: block;
+  margin-top: 40px;
+  margin-bottom: 40px;
+
+  article {
+    font-size: smaller;
+  }
+}
+</style>
