ccceab3fc4e22a410855c26167c5dcf0c1676d7f
Joshua Gato <joshuagato37@gmail.com>
2025-06-28 18:21:34 +0000

    Added card main heading component

---
+++ src/components/headers/CardMainHeading.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <div class="card-main-heading-container">
+    <h1 class="card-main-heading">{{text}}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card-main-heading-container {
+  //margin-top: 45px;
+  //margin-left: 80px;
+
+  .main-heading {
+    font-weight: 700;
+    font-size: 30px;
+  }
+}
+</style>
