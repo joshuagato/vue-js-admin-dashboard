9d968043b4aace36d1a259da89b23629d3523de5
Joshua Gato <joshuagato37@gmail.com>
2025-07-01 17:33:43 +0000

    Imported and used canvasjs/vue-charts

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router';
+import CanvasJSChart from '@canvasjs/vue-charts';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(CanvasJSChart).mount('#app')
